13d76c17fcaac9748e16137c7482205fa0b87d3b
Aleksandar Brayanov <aleksandar@source.network>
2026-07-30 18:49:08 +0100

    chore: resolved linting errors

--- pkg/defra/block_handler.go
+++ pkg/defra/block_handler.go
@@ -1444,5 +1444,3 @@ func (h *BlockHandler) queryBlockNumber(ctx context.Context, order, opName strin
 
 	return 0, errors.NewDocumentNotFound("defra", opName, h.collections.Block, "invalid number type")
 }
-
-
